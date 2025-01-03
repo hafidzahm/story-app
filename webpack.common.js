274564c66ffce461ccce441c0dbd45ca42c39fde
Abdul <hafizhahm123@gmail.com>
2025-01-03 09:34:50 +0700

    form prototype

--- webpack.common.js
+++ webpack.common.js
@@ -26,7 +26,7 @@ const htmlWebpackPluginConfig = {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav m-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/"
+        <a class="nav-link" aria-current="page" href="/"
           >Beranda</a
         >
       </li>
